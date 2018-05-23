8acfe051b98d2e697148af0437ae03a134cb079c
Eric <eventor@ucsc.edu>
2018-05-23 15:55:15 -0700

    Adding Firebase Auth to gradel along with implementation

--- app/src/main/java/info/TheHipMeerkat/MeerGaze/fragment/SocialFragment.java
+++ app/src/main/java/info/TheHipMeerkat/MeerGaze/fragment/SocialFragment.java
@@ -1,14 +1,36 @@
 package info.TheHipMeerkat.MeerGaze.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import info.TheHipMeerkat.MeerGaze.R;
 
 public class SocialFragment extends Fragment {
+    View view;
+    private FirebaseAuth mAuth; //instant of FirebaseAuth
+    private FirebaseUser mUser;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLgnBtn;
+
 
     public SocialFragment() {
         // Required empty public constructor
@@ -24,12 +46,72 @@ public class SocialFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_social, container, false);
+
+        mEmailField = (EditText) view.findViewById(R.id.emailField);
+        mPasswordField = (EditText) view.findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mLgnBtn = (Button) view.findViewById(R.id.button);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    //startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                }
+            }
+        };
+
+        mLgnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startSignIn();
+            }
+        });
+
+        return view;
+    }
+
+    public void onStart(){
+        super.onStart();
+        //updateUI(currentUser); need to create an updateUI method
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn(){
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            Toast.makeText(SocialFragment.this.getActivity(), "Field(s) are empty.", Toast.LENGTH_LONG).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task){
+
+                    if(!task.isSuccessful()){
+                        Toast.makeText(SocialFragment.this.getActivity(), "Sign in problem", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        }
+
+
     }
 }
