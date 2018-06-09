e7089104e639e854f9b935a993f30a1a21007275
Ivaniski <34961876+Ivaniski@users.noreply.github.com>
2018-06-08 17:18:47 -0700

    reorganized fragments

--- app/src/main/java/info/TheHipMeerkat/MeerGaze/fragment/ExploreFragment.java
+++ app/src/main/java/info/TheHipMeerkat/MeerGaze/fragment/ExploreFragment.java
@@ -2,69 +2,54 @@ package info.TheHipMeerkat.MeerGaze.fragment;
 
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.bumptech.glide.Glide;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.List;
 
+import info.TheHipMeerkat.MeerGaze.Movie;
+import info.TheHipMeerkat.MeerGaze.app.MyApplication;
 import info.TheHipMeerkat.MeerGaze.R;
-import info.TheHipMeerkat.MeerGaze.helper.DetailActivity;
-import info.TheHipMeerkat.MeerGaze.helper.ListData;
-//import info.TheHipMeerkat.MeerGaze.helper.SimpleImageListAdapter;
 
 
 public class ExploreFragment extends Fragment {
 
-    FirebaseStorage storage = FirebaseStorage.getInstance();
-    StorageReference storageRef = storage.getReference();
+    private static final String TAG = ExploreFragment.class.getSimpleName();
 
-    private ImageView mImageView;
+    // url to fetch shopping items
+    private static final String URL = "https://firebasestorage.googleapis.com/v0/b/meergaze.appspot.com/o/Loc_Database.json?alt=media&token=e2c4be23-96f4-4dd4-b398-fb025b8242a6";
 
-    public ListView list;
-    public TextView text;
-    public View view;
-
-    public static String[] items = {
-            "http://i.imgur.com/rFLNqWI.jpg",
-            "http://i.imgur.com/C9pBVt7.jpg",
-            "http://i.imgur.com/rT5vXE1.jpg",
-            "http://i.imgur.com/aIy5R2k.jpg",
-            "http://i.imgur.com/MoJs9pT.jpg",
-            "http://i.imgur.com/S963yEM.jpg",
-            "http://i.imgur.com/rLR2cyc.jpg",
-            "http://i.imgur.com/SEPdUIx.jpg",
-            "http://i.imgur.com/aC9OjaM.jpg",
-            "http://i.imgur.com/76Jfv9b.jpg",
-            "http://i.imgur.com/fUX7EIB.jpg",
-            "http://i.imgur.com/syELajx.jpg",
-            "http://i.imgur.com/COzBnru.jpg",
-            "http://i.imgur.com/Z3QjilA.jpg",
-    };
+    private RecyclerView recyclerView;
+    private List<Movie> itemsList;
+    private StoreAdapter mAdapter;
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -82,31 +67,160 @@ public class ExploreFragment extends Fragment {
 
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_explore, container, false);
 
-        //items = getActivity().getResources().getStringArray(R.array.test);
-        list = (ListView) getActivity().findViewById(R.id.data_list_view);
 
-        /*list.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(),
-                android.R.layout.simple_list_item_1 , items));*/
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        itemsList = new ArrayList<>();
+        mAdapter = new StoreAdapter(getActivity(), itemsList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 3);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(8), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setNestedScrollingEnabled(false);
+
+        fetchStoreItems();
 
         return view;
     }
 
-    public void loadImageByInternetUrl(){
+    /**
+     * fetching shopping item by making http call
+     */
+    private void fetchStoreItems() {
+        JsonArrayRequest request = new JsonArrayRequest(URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response == null) {
+                            Toast.makeText(getActivity(), "Couldn't fetch the store items! Pleas try again.", Toast.LENGTH_LONG).show();
+                            return;
+                        }
 
-        //ImageView mImageView =  view.findViewById(R.id.data_list_view);
+                        List<Movie> items = new Gson().fromJson(response.toString(), new TypeToken<List<Movie>>() {
+                        }.getType());
+
+                        itemsList.clear();
+                        itemsList.addAll(items);
+
+                        // refreshing recycler view
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error in getting json
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MyApplication.getInstance().addToRequestQueue(request);
     }
 
-    @Override
-    public void onResume(){
-        super.onResume();
-        loadImageByInternetUrl();
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+
+    /**
+     * RecyclerView adapter class to render items
+     * This class can go into another separate class, but for simplicity
+     */
+    class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder> {
+        private Context context;
+        private List<Movie> movieList;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView name, price;
+            public ImageView thumbnail;
+
+            public MyViewHolder(View view) {
+                super(view);
+                name = view.findViewById(R.id.title);
+                price = view.findViewById(R.id.price);
+                thumbnail = view.findViewById(R.id.thumbnail);
+            }
+        }
+
+
+        public StoreAdapter(Context context, List<Movie> movieList) {
+            this.context = context;
+            this.movieList = movieList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.store_item_row, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, final int position) {
+            final Movie movie = movieList.get(position);
+            holder.name.setText(movie.getTitle());
+            holder.price.setText(movie.getPrice());
+
+            Glide.with(context)
+                    .load(movie.getImage())
+                    .into(holder.thumbnail);
+        }
+
+        @Override
+        public int getItemCount() {
+            return movieList.size();
+        }
 
     }
 
@@ -115,6 +229,7 @@ public class ExploreFragment extends Fragment {
         inflater.inflate(R.menu.menu_main, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
@@ -128,5 +243,4 @@ public class ExploreFragment extends Fragment {
         }
         return true;
     }
-
 }
